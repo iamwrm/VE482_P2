5f6af2743621e6f876fb77dac51f40e0a40b1b1f
Ren Wang <iamwrm@gmail.com>
2018-11-14 20:19:48 +0800

    fix Select empty result and Answer=()

--- src/query/data/SelectQuery.cpp
+++ src/query/data/SelectQuery.cpp
@@ -52,7 +52,7 @@ QueryResult::Ptr SelectQuery::execute()
             }
             outputString = outputStream.str();
         }
-        	return make_unique<SuccessMsgResult>(std::move(outputString));
+        	return make_unique<SuccessMsgResult>(std::move(outputString),1);
     }
     catch (const TableNameNotFound &e) {
         return make_unique<ErrorMsgResult>(qname, this->targetTable, "No such table."s);
